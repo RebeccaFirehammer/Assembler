d2fe91a414ed3858d7da5f6e703146f40077135b
Eriq <zfn653@my.utsa.edu>
2018-04-21 20:12:34 -0500

    Added opCodes for all required instructions

--- src/controller/Translator.java
+++ src/controller/Translator.java
@@ -28,23 +28,156 @@ public class Translator{
 	private void translateInstructions() {
 		for (Instruction instruction: instructions){
 			String command = instruction.getCommand();
+			String destination;
 			switch(command) {
 				case "mov":
-					String source = instruction.getSource();
-					String destination = instruction.getDestination();
-					if(destination.contains("r")) {
-						if(source.contains("r")) {
+					switch(dstSrcPattern(instruction)){
+						case 0:
 							opCode = "80";
-						}else if(source.contains("[")) {
-							opCode = "82";
-						}else {
+							break;
+						case 1:
 							opCode = "81";
-						}
-					}else {
-						opCode = "83";
+							break;
+						case 2:
+							opCode = "82";
+							break;
+						case 3:
+							opCode = "83";
+							break;
 					}
+					break;
+				case "addc":
+					switch(dstSrcPattern(instruction)){
+						case 0:
+							opCode = "10";
+							break;
+						case 1:
+							opCode = "11";
+							break;
+						case 2:
+							opCode = "12";
+							break;
+						case 3:
+							opCode = "13";
+							break;
+					}
+					break;
+				case "subb":
+					switch(dstSrcPattern(instruction)){
+						case 0:
+							opCode = "20";
+							break;
+						case 1:
+							opCode = "21";
+							break;
+						case 2:
+							opCode = "22";
+							break;
+						case 3:
+							opCode = "23";
+							break;
+					}
+					break;
+				case "cmp":
+					switch(dstSrcPattern(instruction)){
+						case 0:
+							opCode = "30";
+							break;
+						case 1:
+							opCode = "31";
+							break;
+						case 2:
+							opCode = "32";
+							break;
+						case 3:
+							opCode = "33";
+							break;
+					}
+					break;
+				case "not":
+					destination = instruction.getDestination();
+					if (destination.contains("["))
+						opCode = "43";
+					else
+						opCode = "40";
+					break;
+				case "and":
+					switch(dstSrcPattern(instruction)){
+						case 0:
+							opCode = "50";
+							break;
+						case 1:
+							opCode = "51";
+							break;
+						case 2:
+							opCode = "52";
+							break;
+						case 3:
+							opCode = "53";
+							break;
+					}
+				break;
+				case "or":
+					switch(dstSrcPattern(instruction)){
+						case 0:
+							opCode = "60";
+							break;
+						case 1:
+							opCode = "61";
+							break;
+						case 2:
+							opCode = "62";
+							break;
+						case 3:
+							opCode = "63";
+							break;
+					}
+					break;
+				case "xor":
+					switch(dstSrcPattern(instruction)){
+						case 0:
+							opCode = "70";
+							break;
+						case 1:
+							opCode = "71";
+							break;
+						case 2:
+							opCode = "72";
+							break;
+						case 3:
+							opCode = "73";
+							break;
+					}
+					break;
+				case "jmp":
+					destination = instruction.getDestination();
+					if (destination.contains("$"))
+						opCode = "B9";
+					else
+						opCode = "B8";
+					break;
+				case "jlo":
+					opCode = "D6";
+					break;
+				case "jhs":
+					opCode = "D7";
+					break;
+				case "jeq":
+					opCode = "D8";
+					break;
+				case "jne":
+					opCode = "D9";
+					break;
+				case "jmi":
+					opCode = "DA";
+					break;
+				case "jpl":
+					opCode = "DB";
+					break;
+				case "nop":
+					opCode = "E0";
+					break;
 					
-				
 			}
 		}
 	}
@@ -78,5 +211,21 @@ public class Translator{
 		line = line.replace(",", " ");
 		line = line.trim();
 		return line;
+	}
+	
+	private int dstSrcPattern(Instruction instruction){
+		String source = instruction.getSource();
+		String destination = instruction.getDestination();
+		if(destination.contains("[")) {
+			return 3;
+		}else {
+			if(source.contains("[")) {
+				return 2;
+			}else if(source.contains("$")) {
+				return 1;
+			}else {
+				return 0;
+			}
+		}
 	}
 }
